d9a13a7a7fb981364a86478a4f38fea317ccf9c8
zhn223 <573094917@qq.com>
2021-10-15 17:03:53 +0800

    注册验证封装

---
+++ src/utils/verification.js
@@ -0,0 +1,13 @@
+/**
+ * 手机号验证
+ * @param {*} value 手机号
+ */
+export funtion checkPhone(value){
+    let regPhone = /^1[3456789]\d{9}$/;
+    return !regPhone.test(value)
+}
+
+/**
+ * 密码验证
+ * @param {*} value 
+ */
